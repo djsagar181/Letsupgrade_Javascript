740a53af8b59036667c7812915900f3dca112c4a
djsagar181 <djsagar181@gmail.com>
2020-07-15 11:11:31 +0530

    Add files via upload
    Hi letsUpgrade,
    I uploaded the files which are executed using the console.
    so i attached png screenshots.

---
+++ day3/Q1_console.js
@@ -0,0 +1,14 @@
+console.log("Sagar");
+-Output-Sagar
+console.warn("This is warning");
+=This is warning
+console.table(['a','b','c']);
+=this will show tabular data
+console.time();
+=This will show time
+console.error("This is error");
+=This is error
+console.clear();
+it will clear the console data.
+console.memory();
+it will dsiplayed the memory.
